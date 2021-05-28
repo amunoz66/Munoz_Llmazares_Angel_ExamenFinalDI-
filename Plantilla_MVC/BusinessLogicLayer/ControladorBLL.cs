7dd76dcc9e782e2150dcbaeda6c7c98151e152e2
as <para@que.com>
2021-05-28 20:10:55 +0200

    subida solucion

---
+++ Plantilla_MVC/BusinessLogicLayer/ControladorBLL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer;
+using ObjetosTransferencia.DTO;
+
+namespace BusinessLogicLayer
+{
+    public class ControladorBLL
+    {
+
+        #region Atributos
+        private List<ClienteDTO> lista;
+
+        #endregion
+
+        #region Constructores
+        public ControladorBLL()
+        {
+
+        }
+        #endregion
+
+        #region Propiedades
+
+        #endregion
+
+        #region Metodos
+
+
+        /// <summary>
+        /// Pedir datos de la lista de clientes a la BD NorthWind SQLServer
+        /// </summary>
+        public List<ClienteDTO> PedirListaClientes()
+        {
+            // TODO: llama al metodo que pide la lista de clientes
+            //throw new Exception();
+            AccesoDatosDAL accesoDAL = new AccesoDatosDAL();
+            lista = accesoDAL.ListadoClientesNorthWind();
+            return lista;
+        }
+
+
+        /// <summary>
+        /// Pedir la lista de pedidos de un clientelista de clientes a la BD NorthWind SQLServer
+        /// </summary>
+        public static List<PedidoDTO> ListarPedidosCliente(String nombreCliente)
+        {
+            // TODO: llamar al metodo que lista los pedidos
+            throw new Exception();
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ Plantilla_MVC/InterfazGrafica/UC/ListaPedidosUC.cs
@@ -0,0 +1,31 @@
+﻿using ObjetosTransferencia.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazGrafica.UC
+{
+    public partial class ListaPedidosUC : UserControl
+    {
+        private List<PedidoDTO> listaPedidos;
+        private DataGridView dg;
+        public ListaPedidosUC(String nombreCliente)
+        {
+            InitializeComponent();
+            // TODO: haz la peticion de los clientes y asociala a la lista y cargalo en el DataGridView
+            dg = new DataGridView();
+            dg.DataSource(dGV_Pedidos);
+         
+        }
+
+        public List<PedidoDTO> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
+
+
+    }
+}
